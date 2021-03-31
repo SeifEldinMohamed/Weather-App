59006e8c3bb9626505ac72921253a716b6514c2d
seif <seifeldinmohamed101@gmail.com>
2021-03-31 02:26:53 +0200

    My Weather

--- app/src/main/java/com/seif/weatherapp/MainActivity.kt
+++ app/src/main/java/com/seif/weatherapp/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.seif.weatherapp
 
-import android.annotation.SuppressLint
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.github.matteobattilana.weather.PrecipType
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.edit_country_name.*
 import org.json.JSONObject
@@ -20,6 +19,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     var City: String = "Cairo,EG"
     val API: String = "5688c6236fceb305faf351a5a473c8ea"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,21 +27,24 @@ class MainActivity : AppCompatActivity() {
         txt_address.setOnClickListener {
             val dialog = AlertDialog.Builder(this)
             val inflater = layoutInflater
-            val dialogLayout = inflater.inflate(R.layout.edit_country_name,null)
+            val dialogLayout = inflater.inflate(R.layout.edit_country_name, null)
             val edittext = dialogLayout.findViewById<EditText>(R.id.edit_countryName)
-            with(dialog){
+            with(dialog) {
                 title = "Enter City and Country Name"
                 setMessage("Ex: Cairo,Egypt")
-                setPositiveButton("Apply"){ dialog, which->
-                    if (edittext.text != null){
+                setPositiveButton("Apply") { dialog, which ->
+                    if (edittext.text != null) {
                         City = edittext.text.trim().toString()
                         weatherTask().execute()
-                    }
-                    else{
-                        Toast.makeText(this@MainActivity, "Please enter City and Country name!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Please enter City and Country name!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
-                setNegativeButton("Cancel"){dialog, which->
+                setNegativeButton("Cancel") { dialog, which ->
 
                 }
                 setView(dialogLayout)
@@ -63,6 +66,7 @@ class MainActivity : AppCompatActivity() {
                 }.show()
             }
         }
+
     }
 
     inner class weatherTask() : AsyncTask<String, Void, String>() {
@@ -118,6 +122,7 @@ class MainActivity : AppCompatActivity() {
                 val weatherDescription = weather.getString("description")
                 // get name of city and country from response jsonObject
                 val address = jsonObj.getString("name") + ", " + sys.getString("country")
+                val weatherMainDescription = weather.getString("main")
 
                 // Populating extracted data into our views
                 txt_address.text = address
@@ -127,12 +132,18 @@ class MainActivity : AppCompatActivity() {
                 txt_minT.text = tempMin
                 txt_maxT.text = tempMax
                 txt_Vsunrise.text =
-                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH)
+                        .format(Date(sunrise * 1000))
                 txt_Vsunset.text =
-                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH)
+                        .format(Date(sunset * 1000))
                 txt_Vwind.text = windSpeed
                 txt_Vpressure.text = pressure
                 txt_Vhumidity.text = humidity
+
+                // Weather Animation
+                changeWeather(weatherMainDescription)
+
                 // Views populated, Hiding the progressBar, Showing the main design.
                 progressBar.visibility = View.GONE
                 MainContainer.visibility = View.VISIBLE
@@ -141,6 +152,15 @@ class MainActivity : AppCompatActivity() {
                 progressBar.visibility = View.GONE
                 txt_Error.visibility = View.VISIBLE
             }
+        }
+    }
+
+    private fun changeWeather(weather: String) {
+        when (weather) {
+            "Clear" -> weatherView.setWeatherData(PrecipType.CLEAR)
+            "Rain" -> weatherView.setWeatherData(PrecipType.RAIN)
+            "Snow" -> weatherView.setWeatherData(PrecipType.SNOW)
+            else -> weatherView.setWeatherData(PrecipType.CLEAR)
         }
     }
 }
